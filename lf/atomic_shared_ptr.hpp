e91a26f9cdfcfb523e518acd11846980e88d9d5c
Lingxi Li <lilingxi.cs@gmail.com>
2018-02-22 12:07:21 +0800

    Implement atomic_shared_ptr modify

--- lf/atomic_shared_ptr.hpp
+++ lf/atomic_shared_ptr.hpp
@@ -14,10 +14,8 @@ namespace atomic_shared_ptr_impl {
 
 using shared_ptr_impl::block;
 
-static constexpr auto one_stagecnt = std::uint64_t(1) << 32;
-
 // Works with block.
-// Actual staged reference count = stagecnt / one_stagecnt.
+// Actual staged reference count = stagecnt / one_stagecnt (i.e., uint64_t(1) << 32).
 template <typename T>
 struct counted_ptr {
   std::uint64_t stagecnt;
@@ -30,6 +28,7 @@ template <typename T>
 class atomic_shared_ptr {
   using shared_ptr_t = shared_ptr<T>;
   using counted_ptr = atomic_shared_ptr_impl::counted_ptr<T>;
+  static constexpr auto one_stagecnt = std::uint64_t(1) << 32;
 
 public:
   // copy control
@@ -55,13 +54,68 @@ public:
   }
 
   // modify
-  void operator=(shared_ptr_t p_);
+  void operator=(shared_ptr_t p) {
+    counted_ptr newp{0, std::exchange(p.pblock, nullptr)};
+    auto oldp = pblock.exchange(newp);
+    if (oldp.pblock) {
+      oldp.pblock->cnt += oldp.stagecnt;
+      p.pblock = oldp.pblock;
+    }
+  }
 
-  shared_ptr_t exchange(shared_ptr_t p);
-  bool compare_exchange_weak(shared_ptr_t& expect, const shared_ptr_t& desire);
-  bool compare_exchange_weak(shared_ptr_t& expect, shared_ptr_t&& desire);
-  bool compare_exchange_strong(shared_ptr_t& expect, const shared_ptr_t& desire);
-  bool compare_exchange_strong(shared_ptr_t& expect, shared_ptr_t&& desire);
+  shared_ptr_t exchange(shared_ptr_t p) {
+    counted_ptr newp{0, std::exchange(p.pblock, nullptr)};
+    auto oldp = pblock.exchange(newp);
+    if (oldp.pblock) {
+      oldp.pblock->cnt += oldp.stagecnt;
+      return oldp.pblock;
+    }
+    return {};
+  }
+
+  bool compare_exchange_weak(shared_ptr_t& expect, const shared_ptr_t& desire) {
+    auto oldp = copy_ptr();
+    if (oldp.pblock != expect.pblock) {
+      expect = oldp.pblock;
+      return false;
+    }
+    counted_ptr newp{0, desire.pblock};
+    if (pblock.compare_exchange_strong(oldp, newp)) {
+      if (oldp.pblock) oldp.pblock->cnt += oldp.stagecnt - 2;
+      if (desire) ++desire.pblock->cnt;
+      return true;
+    }
+    if (expect) --expect.pblock->cnt;
+    return false;
+  }
+
+  bool compare_exchange_weak(shared_ptr_t& expect, shared_ptr_t&& desire) {
+    auto oldp = copy_ptr();
+    if (oldp.pblock != expect.pblock) {
+      expect = oldp.pblock;
+      return false;
+    }
+    counted_ptr newp{0, desire.pblock};
+    if (pblock.compare_exchange_strong(oldp, newp)) {
+      if (oldp.pblock) oldp.pblock->cnt += oldp.stagecnt - 2;
+      desire.pblock = nullptr;
+      return true;
+    }
+    if (expect) --expect.pblock->cnt;
+    return false;
+  }
+
+  bool compare_exchange_strong(shared_ptr_t& expect, const shared_ptr_t& desire) {
+    auto p = expect.pblock;
+    while (!compare_exchange_weak(expect, desire) && p == expect.pblock);
+    return p == expect.pblock;
+  }
+
+  bool compare_exchange_strong(shared_ptr_t& expect, shared_ptr_t&& desire) {
+    auto p = expect.pblock;
+    while (!compare_exchange_weak(expect, std::move(desire)) && p == expect.pblock);
+    return p == expect.pblock;
+  }
 
   // observer
   bool is_lock_free() const;
@@ -78,6 +132,7 @@ private:
       pp.stagecnt += one_stagecnt;
     }
     while (!pblock.compare_exchange_weak(p, pp));
+    pp.pblock->cnt += -one_stagecnt + 1;
     return pp;
   }
 };
