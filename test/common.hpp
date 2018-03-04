0ab8b15e6096805e997c0e9e6fd07bf6c549c549
Lingxi Li <lilingxi.cs@gmail.com>
2018-03-04 22:15:19 +0800

    Fix test infra bug

--- test/common.hpp
+++ test/common.hpp
@@ -4,8 +4,8 @@
 #include <cassert>
 #include <iostream>
 
-#define LF_TEST_BEGIN(name) void name_test() {
+#define LF_TEST_BEGIN(name) void name##_test() {
 #define LF_TEST_END std::cout << __func__ << " passed" << std::endl; }
-#define LF_TEST_RUN(name) name_test();
+#define LF_TEST_RUN(name) name##_test();
 
 #endif // LF_TEST_COMMON_HPP
