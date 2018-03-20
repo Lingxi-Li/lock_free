2d0cb1b0ec87d559d0f31d92473321d5066ee44b
Lingxi Li <lilingxi.cs@gmail.com>
2018-03-20 21:20:49 +0800

    Add REQUIRE_SAME_T()

--- test/test.hpp
+++ test/test.hpp
@@ -1,2 +1,8 @@
 #define CATCH_CONFIG_DISABLE_MATCHERS
 #include "catch.hpp"
+
+#include <type_traits>
+
+#define REQUIRE_SAME_T(...) static_assert( \
+    std::is_same<__VA_ARGS__>{}, \
+    #__VA_ARGS__ " are not the same.")
