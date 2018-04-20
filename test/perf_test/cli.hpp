fb70b6a3c810ad7711887f9a71e46025ffba98a6
Lingxi Li <lilingxi.cs@gmail.com>
2018-04-20 21:36:55 +0800

    Factor cli.hpp

---
+++ test/perf_test/cli.hpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+
+#define MAIN(...) \
+  void main_(__VA_ARGS__); \
+  int main(int argc, char* argv[]) { \
+    impl::guarded_run(main_, argc, (const char**)argv, #__VA_ARGS__); \
+  } \
+  void main_(__VA_ARGS__)
+
+template <typename T, T def>
+struct optional {
+  operator T&() noexcept {
+    return value;
+  }
+  T value{def};
+};
+
+namespace impl {
+
+template <typename T>
+void set_arg(T& arg, const char* str) {
+  if (!(std::istringstream(str) >> arg)) {
+    throw std::invalid_argument(
+      std::string("Invalid argument: ") + str);
+  }
+}
+
+inline void set_arg(std::string& arg, const char* str) {
+  arg = str;
+}
+
+inline void set_args(const char** p, const char** end) {
+  if (p != end) {
+    throw std::invalid_argument("Too many arguments.");
+  }
+}
+
+template <typename T, typename... Us>
+void set_args(const char** p, const char** end, T& arg, Us&... args) {
+  if (p == end) throw std::invalid_argument("Too few arguments.");
+  set_arg(arg, *p++);
+  set_args(p, end, args...);
+}
+
+template <typename T, T def, typename... Us>
+void set_args(const char** p, const char** end, optional<T, def>& arg, Us&... args) {
+  if (p == end) return;
+  set_arg(arg, *p++);
+  set_args(p, end, args...);
+}
+
+template <typename... Us>
+void guarded_run(
+ void(*f)(Us...), int argc, const char** argv, const char* params) noexcept {
+  try {
+    auto p = argv + 1, end = argv + argc;
+    std::tuple<Us...> args;
+    std::apply([p, end](auto&... args) {
+      set_args(p, end, args...);
+    }, args);
+    std::apply(f, std::move(args));
+  }
+  catch (const std::invalid_argument& e) {
+    std::cout << e.what() << '\n'
+              << "Usage: (" << params << ')' << std::endl;
+  }
+  catch (const std::exception& e) {
+    std::cout << "std::exception caught with message:\n"
+              << e.what() << std::endl;
+  }
+  catch (...) {
+    std::cout << "Unknown exception caught." << std::endl;
+  }
+}
+
+} // namespace impl
--- test/perf_test/perf.hpp
+++ test/perf_test/perf.hpp
@@ -1,62 +1,7 @@
-#include <cstddef>
+#include "cli.hpp"
+
 #include <atomic>
 #include <chrono>
-#include <stdexcept>
-#include <string>
-#include <iostream>
-#include <sstream>
-#include <tuple>
-#include <utility>
-
-#define MAIN(...) \
-  void main_(__VA_ARGS__); \
-  int main(int argc, char* argv[]) { \
-    impl::guarded_run(main_, argc, argv, #__VA_ARGS__); \
-  } \
-  void main_(__VA_ARGS__)
-
-namespace impl {
-
-template <typename T>
-void set_arg(T& arg, char* str) {
-  if (!(std::istringstream(str) >> arg)) {
-    throw std::invalid_argument(
-      std::string("Invalid argument: ") + str);
-  }
-}
-
-inline void set_arg(std::string& arg, char* str) {
-  arg = str;
-}
-
-template <typename... Us, std::size_t... Idx>
-void set_args(
- std::tuple<Us...>& args, std::index_sequence<Idx...>, char* argv[]) {
-  (set_arg(std::get<Idx>(args), argv[Idx]), ...);
-}
-
-template <typename... Args>
-void guarded_run(void(*p)(Args...), int argc, char* argv[], const char* params) noexcept {
-  --argc, ++argv;
-  try {
-    if (argc != sizeof...(Args)) {
-      throw std::invalid_argument("Invalid number of arguments.");
-    }
-    std::tuple<Args...> args;
-    set_args(args, std::index_sequence_for<Args...>{}, argv);
-    std::apply(p, std::move(args));
-  }
-  catch (const std::exception& e) {
-    std::cout
-      << "std::exception caught with message:\n" << e.what() << '\n'
-      << "Usage: (" << params << ')' << std::endl;
-  }
-  catch (...) {
-    std::cout << "Unknown exception caught." << std::endl;
-  }
-}
-
-} // namespace impl
 
 inline auto tick() {
   return std::chrono::high_resolution_clock::now()
