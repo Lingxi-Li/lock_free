6cbc6cc400290d9047a7ac5381aa8bb0586cba09
Lingxi Li <lilingxi.cs@gmail.com>
2018-04-24 20:31:56 +0800

    Factor simulator::per_thread_data

--- test/perf_test/simulator.hpp
+++ test/perf_test/simulator.hpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <functional>
 #include <initializer_list>
+#include <iostream>
 #include <random>
 #include <thread>
 #include <vector>
@@ -30,6 +31,7 @@ public:
    unsigned margin,
    std::initializer_list<measure_fn> fn_list) {
     validate_thread_cnt(thread_cnt);
+    std::cout << "Configuring..." << std::endl;
     simulator::thread_cnt = thread_cnt;
     fn.assign(fn_list);
     op_cnt = fn.size();
@@ -39,45 +41,50 @@ public:
 
   static void kickoff() {
     if (thread_cnt == 0) ERROR("simulator not configured yet.");
+    std::cout << "Running..." << std::endl;
     data.resize(thread_cnt);
-    runner.reserve(thread_cnt - 1);
+    threads.reserve(thread_cnt - 1);
     for (auto i = 0u; i < thread_cnt - 1; ++i) {
-      runner.emplace_back(&simulator::wait_and_run, &data[i]);
+      threads.emplace_back(&per_thread_data::wait_and_run, &data[i]);
     }
     data.back().signal_and_run();
-    for_each_element(&std::thread::join, runner.begin(), runner.end());
-  }
-
-  simulator():
-   measures(op_cnt) {
-    auto len = reps + margin * 2;
-    opseq.reserve(op_cnt * len);
-    for (auto i = 0u; i < op_cnt; ++i) {
-      opseq.insert(opseq.end(), len, i);
-      measures[i].reserve(len);
-    }
-    std::shuffle(opseq.begin(), opseq.end(), rnd);
+    for_each_element(&std::thread::join, threads.begin(), threads.end());
   }
 
 private:
-  void wait_and_run() {
-    sync.wait();
-    run();
-  }
+  class per_thread_data {
+  public:
+    std::vector<std::uint8_t> opseq; // op_cnt * (reps + margin * 2)
+    matrix<measure_t> measures;      // op_cnt * (reps + margin * 2)
 
-  void signal_and_run() {
-    sync.signal(thread_cnt - 1);
-    run();
-  }
-
-  void run() {
-    for (auto op : opseq) {
-      measures[op].push_back(fn[op]());
+    per_thread_data():
+     measures(op_cnt) {
+      auto len = reps + margin * 2;
+      opseq.reserve(op_cnt * len);
+      for (auto i = 0u; i < op_cnt; ++i) {
+        opseq.insert(opseq.end(), len, i);
+        measures[i].reserve(len);
+      }
+      std::shuffle(opseq.begin(), opseq.end(), rnd);
     }
-  }
 
-  std::vector<std::uint8_t> opseq; // op_cnt * (reps + margin * 2)
-  matrix<measure_t> measures;      // op_cnt * (reps + margin * 2)
+    void wait_and_run() {
+      sync.wait();
+      run();
+    }
+
+    void signal_and_run() {
+      sync.signal(thread_cnt - 1);
+      run();
+    }
+
+  private:
+    void run() {
+      for (auto op : opseq) {
+        measures[op].push_back(fn[op]());
+      }
+    }
+  };
 
   inline static unsigned thread_cnt;
   inline static std::vector<measure_fn> fn;
@@ -85,10 +92,10 @@ private:
   inline static unsigned reps;
   inline static unsigned margin;
 
-  inline static std::vector<simulator> data;
-  inline static std::vector<std::thread> runner;
+  inline static std::vector<per_thread_data> data;
+  inline static std::vector<std::thread> threads;
 
-  inline static std::mt19937 rnd{std::mt19937::default_seed};
+  inline static std::mt19937 rnd;
   inline static sync_point sync;
 };
 
