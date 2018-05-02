edbf7e02827749a7224d2407979deeb086f8c128
Lingxi Li <lilingxi.cs@gmail.com>
2018-05-02 15:07:40 +0800

    Draft new allocator interface

--- lf/allocator.hpp
+++ lf/allocator.hpp
@@ -1,135 +1,35 @@
-#ifndef LF_ALLOCATOR_HPP
-#define LF_ALLOCATOR_HPP
+#ifndef LF_ALLOCATOR_HPP_
+#define LF_ALLOCATOR_HPP_
 
-#include "common.hpp"
+#include "split_ref.hpp"
 
-#include <cstddef>
+#include <cstdint>
 #include <atomic>
-#include <memory>
 
-namespace lf {
-
-namespace allocator_impl {
-
-template <typename T>
-struct node {
-  T data;
-  std::atomic<node*> next;
-};
-
-template <typename T>
-struct mem {
-  using node = node<T>;
-
-  static node* allocate(std::size_t n) {
-    return (node*)operator new(sizeof(node) * n);
-  }
-
-  struct deallocator {
-    void operator()(node* p) const noexcept {
-      operator delete(p);
-    }
-  };
-};
-
-} // namespace allocator_impl
-
-template <typename T>
-struct deleter;
+#include "prolog.inc"
 
 template <typename T>
 class allocator {
-  using mem = allocator_impl::mem<T>;
-  using counted_ptr = lf::counted_ptr<typename mem::node>;
-  using deallocator = typename mem::deallocator;
-
 public:
-  using deleter = lf::deleter<T>;
-  using node = typename mem::node;
+  struct node: T {
+    std::atomic<node*> next;
+  };
 
-  // copy control
+  allocator() noexcept = default;
+  explicit allocator(std::size_t capacity);
+  ~allocator();
   allocator(const allocator&) = delete;
   allocator& operator=(const allocator&) = delete;
 
-  // construct
-  allocator() noexcept = default;
-
-  explicit allocator(std::size_t capacity):
-    nodes(mem::allocate(capacity)),
-    head({nodes.get()}) {
-    link_up(capacity);
-  }
-
-  // modifier
-  void reset(std::size_t capacity) {
-    nodes.reset(mem::allocate(capacity));
-    head.store({nodes.get()}, rlx);
-    link_up(capacity);
-  }
-
-  node* try_allocate_node() noexcept {
-    counted_ptr oldhead(head.load(acq)), newhead;
-    do {
-      if (!oldhead.p) return nullptr;
-      newhead.p = oldhead.p->next.load(rlx);
-      newhead.trefcnt = oldhead.trefcnt + 1;
-    }
-    while (!head.compare_exchange_weak(oldhead, newhead, rlx, acq));
-    return oldhead.p;
-  }
-
-  T* try_allocate() noexcept {
-    return (T*)try_allocate_node();
-  }
-
-  void deallocate(node* pn) noexcept {
-    counted_ptr oldhead(head.load(rlx)), newhead{pn};
-    do {
-      pn->next.store(oldhead.p, rlx);
-      newhead.trefcnt = oldhead.trefcnt;
-    }
-    while (!head.compare_exchange_weak(oldhead, newhead, rel, rlx));
-  }
-
-  void deallocate(T* p) noexcept {
-    deallocate((node*)p);
-  }
-
-  deleter get_deleter() const noexcept {
-    return {const_cast<allocator&>(*this)};
-  }
+  void reset(std::size_t capacity);
+  node* try_allocate() noexcept;
+  void deallocate(void* p) noexcept;
 
 private:
-  void link_up(std::size_t capacity) noexcept {
-    auto p = nodes.get();
-    auto last = p + capacity - 1;
-    while (p < last) {
-      p->next.store(p + 1, rlx);
-      ++p;
-    }
-    p->next.store(nullptr, rlx);
-  }
-
-  std::unique_ptr<node, deallocator> nodes{};
-  std::atomic<counted_ptr> head{};
-};
-// class allocator
-
-template <typename T>
-struct deleter {
-  using node = typename allocator<T>::node;
-  allocator<T>& alloc;
-
-  void operator()(node* p) const noexcept {
-    operator()((T*)p);
-  }
-
-  void operator()(T* p) const noexcept {
-    p->~T();
-    alloc.deallocate(p);
-  }
+  node* backup{};
+  std::atomic_counted_ptr<node> head{};
 };
 
-} // namespace lf
+#include "epilog.inc"
 
-#endif // LF_ALLOCATOR_HPP
+#endif // LF_ALLOCATOR_HPP_
