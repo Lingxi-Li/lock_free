1886f1e7322d633f68a421a63f9d0792b3f3fa19
Lingxi Li <lilingxi.cs@gmail.com>
2018-05-04 19:39:56 +0800

    Implement allocator

--- lf/allocator.hpp
+++ lf/allocator.hpp
@@ -1,33 +1,85 @@
 #ifndef LF_ALLOCATOR_HPP_
 #define LF_ALLOCATOR_HPP_
 
+#include "memory.hpp"
 #include "split_ref.hpp"
+#include "utility"
 
 #include <cstdint>
 #include <atomic>
+#include <utility>
 
 #include "prolog.inc"
 
 template <typename T>
 class allocator {
 public:
-  struct node: T {
+  struct node {
+    T val;
     std::atomic<node*> next;
   };
 
   allocator() noexcept = default;
-  explicit allocator(std::size_t capacity);
-  ~allocator();
+
+  explicit allocator(std::size_t capacity):
+   backup(capacity ? allocate<node>(capacity) : nullptr),
+   head(cp_t{backup}) {
+    if (backup) link(capacity);
+  }
+
+  ~allocator() {
+    deallocate(backup);
+  }
+
   allocator(const allocator&) = delete;
   allocator& operator=(const allocator&) = delete;
 
-  void reset(std::size_t capacity);
-  node* try_allocate() noexcept;
-  void deallocate(void* p) noexcept;
+  void reset(std::size_t capacity) {
+    if (capacity == 0) {
+      deallocate(std::exchange(backup, nullptr));
+      head.store({}, rlx);
+    }
+    else {
+      deallocate(std::exchange(backup, allocate<node>(capacity)));
+      head.store({backup}, rlx);
+      link(capacity);
+    }
+  }
+
+  node* try_allocate() noexcept {
+    cp_t newhd, hd(head.load(acq));
+    do {
+      if (!hd.ptr) return nullptr;
+      newhd.ptr = hd.ptr->next.load(rlx);
+      newhd.cnt = hd.cnt + 1;
+    }
+    while (!head.compare_exchange_weak(hd, newhd, rlx, acq));
+    return hd.ptr;
+  }
+
+  void deallocate(node* p) noexcept {
+    cp_t newhd{p}, hd(head.load(rlx));
+    do {
+      p->next.store(hd.ptr, rlx);
+      newhd.cnt = hd.cnt;
+    }
+    while (!head.compare_exchange_weak(hd, newhd, rel, rlx));
+  }
 
 private:
+  using cp_t = counted_ptr<node>;
+
+  void link(std::size_t capacity) noexcept {
+    auto p = backup, last = backup + capacity - 1;
+    while (p != last) {
+      init_no_catch(&p->next, p + 1);
+      ++p;
+    }
+    init_no_catch(&p->next, nullptr);
+  }
+
   node* backup{};
-  std::atomic_counted_ptr<node> head{};
+  std::atomic<cp_t> head{cp_t{}};
 };
 
 #include "epilog.inc"
