f4b3c3089734fe8a263146d6eea6894f1f7dae48
Lingxi Li <lilingxi.cs@gmail.com>
2018-05-13 11:25:22 +0800

    Add stack perf test

--- test/perf_test/stack.cpp
+++ test/perf_test/stack.cpp
@@ -1,27 +1,64 @@
 #include "cli.hpp"
+#include "libtag.hpp"
 #include "simulator.hpp"
 
-#include "../../lf/stack.hpp"
-#include "boost/lockfree/stack.hpp"
+#include <lf/stack.hpp>
+#include <boost/lockfree/stack.hpp>
 
-MAIN(unsigned thread_cnt) {
-  simulator::configure(thread_cnt, 1_M, 1_K, {});
-
-  // boost::lockfree::stack<int> stk(1_M);
-  // for (auto i = 0; i < 1_M; ++i) {
-  //   stk.bounded_push(std::move(i));
-  // }
-
-  // int v;
-  // stk.pop(v);
-  // stk.pop(v);
-  // std::cout << v << std::endl;
-
-  lf::stack<int> stk(1_M);
-  for (auto i = 0; i < 1_M; ++i) {
+std::vector<measure_fn> get_lf_fn(unsigned op_cnt, unsigned margin) {
+  auto len = op_cnt + 2 * margin;
+  static lf::stack<unsigned> stk(2 * len);
+  for (auto i = 0u; i < len; ++i) {
     stk.try_push(std::move(i));
   }
+  return {
+    []() noexcept {
+      auto di = tick::now();
+      stk.try_push(0u);
+      auto da = tick::now();
+      return tp_pr{di, da};
+    },
+    []() noexcept {
+      auto di = tick::now();
+      (void)stk.try_pop();
+      auto da = tick::now();
+      return tp_pr{di, da};
+    }
+  };
+}
 
-  stk.try_pop();
-  std::cout << *stk.try_pop() << std::endl;
+std::vector<measure_fn> get_boost_fn(unsigned op_cnt, unsigned margin) {
+  auto len = op_cnt + 2 * margin;
+  static boost::lockfree::stack<unsigned> stk(2 * len);
+  for (auto i = 0u; i < len; ++i) {
+    stk.bounded_push(i);
+  }
+  return {
+    [] {
+      auto di = tick::now();
+      stk.bounded_push(0u);
+      auto da = tick::now();
+      return tp_pr{di, da};
+    },
+    [] {
+      auto di = tick::now();
+      unsigned ret;
+      (void)stk.pop(ret);
+      auto da = tick::now();
+      return tp_pr{di, da};
+    }
+  };
+}
+
+MAIN(
+ lib tag,
+ unsigned thread_cnt,
+ optional<unsigned, 10_M> op_cnt,
+ optional<unsigned,  1_M> margin) {
+  auto get_fn = tag == lib::lf ? &get_lf_fn : &get_boost_fn;
+  auto file_name = "stack_" + to_str(tag) + ".csv";
+
+  simulator::configure(thread_cnt, op_cnt, margin, get_fn(op_cnt, margin));
+  simulator::kickoff();
+  simulator::write_result(file_name);
 }
