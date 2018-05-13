f6b420b6723d374ee03433644b666e8a7fe803cb
Lingxi Li <lilingxi.cs@gmail.com>
2018-05-13 16:46:39 +0800

    Install boost

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,6 @@
 case $TRAVIS_OS_NAME in
 linux)
   BUILD=$CXX-$VER
-  EXTRA=-latomic
   COV=gcov-$VER
   sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
   sudo apt-get update -qq
@@ -9,7 +8,6 @@ linux)
   ;;
 osx)
   BUILD=$CXX-mp-$VER
-  EXTRA=
   COV="llvm-cov-mp-$VER gcov"
   export COLUMNS=80
   curl -LO https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macports-ci
@@ -19,6 +17,18 @@ osx)
   sudo port install $CC-$VER
   ;;
 esac
-UNIT_TEST=$(pwd)/test/unit_test
+
+ROOT=$(pwd)
+UNIT_TEST=$ROOT/test/unit_test
+PERF_TEST=$ROOT/test/perf_test
+
+curl -LO https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
+tar --gzip -xf boost*
+cp -R boost*/boost $PERF_TEST/boost
+
+ESSENTIAL="-std=c++17 -Werror -pedantic -pedantic-errors -Wall -Wextra"
+DEBUG="$ESSENTIAL -O0 -g3 -coverage"
+PERF="$ESSENTIAL -I$ROOT -I$PERF_TEST -O3"
+
 mkdir bin
 cd bin
